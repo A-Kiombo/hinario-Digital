b6b5a14b4049a75c7ea6f0b1f12038864c808b14
A-Kiombo <anibalkiombo0@gmail.com>
2026-03-19 16:03:07 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,20 @@
-const CACHE_NAME = 'hinario-v1.2'; // Mudei a versão para forçar o update
+const CACHE_NAME = 'hinario-v1';
 const assets = [
   './',
   './index.html',
-  './manifest.json',
-  './CBA.JPEG',
   './hinos.json',
-  './hinosportugues.json'
+  './hinosportugues.json',
+  './CBA.JPEG'
 ];
 
-// Instalação e Cache
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Instalando cache...');
-      // Usamos o map para que, se um ficheiro falhar, os outros continuem
-      return Promise.all(
-        assets.map(url => {
-          return cache.add(url).catch(err => console.log('Erro ao carregar:', url, err));
-        })
-      );
-    })
-  );
-  self.skipWaiting();
-});
-
-// Ativação (Limpa caches antigos)
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
 });
 
-// Resposta às requisições
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
